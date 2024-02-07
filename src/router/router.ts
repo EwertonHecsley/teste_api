eba2b70e84bf60b7c04adc376e85aee630124bca
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-06 21:57:06 -0300

    adicionando mais uma rota teste

--- src/router/router.ts
+++ src/router/router.ts
@@ -7,4 +7,8 @@ router.get('/', (_req, res) => {
     return res.status(200).json({ mensagem: 'Hello World!' })
 });
 
+router.get('/teste', (req, res) => {
+    return res.json({ mensagem: 'Passou no teste.' });
+})
+
 export default router;
